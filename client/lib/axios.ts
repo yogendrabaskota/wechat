fc271cda5f0ce4d55aab1f86248957e5f52c6f1b
yogendrabaskota <yogendrabaskota18@gmail.com>
2026-02-21 20:36:10 +0545

    bug fix

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -1,9 +1,18 @@
 import axios from 'axios';
+import { useAuthStore } from '@/store/authStore';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
+});
+
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
